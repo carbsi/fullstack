9c44347fcbce9e0530021dcf90b1c2d0e1af6ff1
karpot <otto.jh.karppinen@student.jyu.fi>
2024-07-15 10:36:07 +0300

    ensimmäinen osa

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  const course = 'Half Stack application development';
+  ;
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+  return (
+    <div>
+      <Header course={course} />
+    </div>
+  );
+};
+
+export default App;
