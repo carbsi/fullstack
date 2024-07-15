f37d60fe8561535dc45754d3a8481b6e7c2f39c9
karpot <otto.jh.karppinen@student.jyu.fi>
2024-07-15 14:21:56 +0300

    tehtävät 1.1-1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import React from 'react';
-
 const App = () => {
   const course = 'Half Stack application development';
   const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
   ];
-  ;
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
 };
 
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exercises}
-        </p>
+      {parts.map(part => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
       ))}
     </div>
   );
@@ -41,7 +31,7 @@ const Total = ({ parts }) => {
 
   return (
     <div>
-      <Header course={course} />
+      <h1>{course}</h1>
       <Content parts={parts} />
       <Total parts={parts} />
     </div>
