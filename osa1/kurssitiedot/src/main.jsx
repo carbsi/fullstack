45b3c9f11a0cf9fa3a361f5fa584ecae77ed518e
karpot <otto.jh.karppinen@student.jyu.fi>
2024-07-16 08:11:44 +0300

    Kurssitiedot 1.3 step3 | console.log lisääminen ja muuttujamäärittelyjen oliointi

--- osa1/kurssitiedot/src/main.jsx
+++ osa1/kurssitiedot/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
 
+// Renderöi App-komponentin juurielementtiin
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
