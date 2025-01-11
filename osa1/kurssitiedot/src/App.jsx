3983fb15eaff99e5f74cd4ef638c3b4728fca4f8
Otto Karppinen <ottokarpp@gmail.com>
2025-01-12 00:06:04 +0200

    Kurssitiedot tehtävät 2.1-2.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,66 +1,58 @@
 import React from 'react';
+import Course from './Course'; // Tuodaan Course-komponentti
 
-// App-komponentti sisältää koko sovelluksen
 const App = () => {
-// kurssille nimi
-  const course = 'Half Stack application development';
-  // kurssin osat ja harkkojen määrät
-  const parts = [
+  // Kurssit taulukossa
+  const courses = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
   ];
 
-  // Part-komponentti esittää yksittäisen osan
-  const Part = ({ name, exercises }) => {
-    console.log('Part props:', { name, exercises });
-    return (
-      <p>
-        {name} {exercises}
-      </p>
-    );
-  };
-
-  // Content-komponentti esittää kurssin osat // kolmen alkion oletus
-  const Content = ({ parts }) => {
-    console.log('Content props:', { parts });
-    return (
-      <div> 
-          <Part name={parts[0].name} exercises={parts[0].exercises} /> 
-          <Part name={parts[1].name} exercises={parts[1].exercises} />
-          <Part name={parts[2].name} exercises={parts[2].exercises} />
-      </div>
-    );
-  };
-
-  // Header-komponentti esittää kurssin nimen
-  const Header = ({ course }) => {
-    console.log('Header props:', { course });
-    return <h1>{course}</h1>;
-  };
-
-  // Total-komponentti laskee ja esittää harjoitusten kokonaismäärän
-  const Total = ({ parts }) => {
-    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-    console.log('Total props:', { parts });
-    return <p>Number of exercises {totalExercises}</p>;
-  };
-
-  // Koko sovelluksen rakenne palautuu
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
